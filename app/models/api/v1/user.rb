a436f57e81434ecac1a1b66ddb43086dbd7bfa42
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-05-17 20:39:00 -0400

    Add User model

---
+++ app/models/api/v1/user.rb
@@ -0,0 +1,16 @@
+class Api::V1::User < ApplicationRecord
+  before_create :maybe_assign_id
+
+  has_secure_password
+
+  validates :id, presence: true, on: :update
+  validates :id, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def maybe_assign_id
+    self.id = SecureRandom.uuid if id.blank?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_000832) do
+# Could not dump table "api_v1_users" because of following StandardError
+#   Unknown type 'uuid' for column 'id'
+
 end
