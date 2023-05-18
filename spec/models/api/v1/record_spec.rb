9fee399c467a3425292a371680e5f6a37152b925
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-05-18 18:50:48 -0400

    Add test for record an User models

--- spec/models/api/v1/record_spec.rb
+++ spec/models/api/v1/record_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:travelled_distance) }
+    it { should validate_numericality_of(:travelled_distance).is_greater_than(0) }
+    it { should validate_presence_of(:user_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user).class_name('Api::V1::User') }
+  end
 end
